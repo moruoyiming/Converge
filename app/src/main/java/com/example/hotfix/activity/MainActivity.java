a0f66e4f389356536bc6cae184c5051396d1c46b
jianruilin <798774875@qq.com>
2020-06-04 09:37:35 +0800

    网络请求 基于retrofit请求代码

--- app/src/main/java/com/example/hotfix/activity/MainActivity.java
+++ app/src/main/java/com/example/hotfix/activity/MainActivity.java
@@ -9,36 +9,36 @@ import android.view.View;
 import com.cocos.basewebview.WebviewActivity;
 import com.example.hotfix.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Utils.what();
-        findViewById(R.id.openWeb1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        findViewById(R.id.openWeb1).setOnClickListener(this);
+        findViewById(R.id.openWeb2).setOnClickListener(this);
+        findViewById(R.id.network).setOnClickListener(this);
+        findViewById(R.id.lf).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.openWeb1:
                 WebviewActivity.startCommonWeb(MainActivity.this, "腾讯网", "https://xw.qq.com/?f=qqcom");
-            }
-        });
-
-        findViewById(R.id.openWeb2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // for baselevel
-                //RemoteCommonWebActivity.start(MainActivity.this, "AIDL测试", DWebView.CONTENT_SCHEME + "aidl.html");
-
-                // for account level
+                break;
+            case R.id.openWeb2:
                 WebviewActivity.startCommonWeb(MainActivity.this, "AIDL测试", "file:///android_asset/"+ "aidl.html");
-            }
-        });
-        findViewById(R.id.network).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.network:
                 Intent intent = new Intent(MainActivity.this, NetworkActivity.class);
                 startActivity(intent);
-            }
-        });
+                break;
+            case R.id.lf:
+                Intent intent2 = new Intent(MainActivity.this, AActivity.class);
+                startActivity(intent2);
+                break;
+        }
     }
 }
