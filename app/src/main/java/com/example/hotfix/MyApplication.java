d61bcd3bdbe491642c4efbbd34591f8127e5eb2b
jianruilin <798774875@qq.com>
2020-06-08 18:20:21 +0800

    集成google 广告

--- app/src/main/java/com/example/hotfix/MyApplication.java
+++ app/src/main/java/com/example/hotfix/MyApplication.java
@@ -1,10 +1,15 @@
 package com.example.hotfix;
 
 import android.app.Application;
+import android.widget.Toast;
 
+import com.cocos.base.utils.ToastUtil;
 import com.cocos.basewebview.mainprocess.CommandsManager;
 import com.example.hotfix.command.AccountLevelCommands;
 import com.example.hotfix.command.BaseLevelCommands;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MyApplication extends Application {
 
@@ -14,5 +19,11 @@ public class MyApplication extends Application {
         CommandsManager.getInstance().registerCommand(new AccountLevelCommands());
         CommandsManager.getInstance().registerCommand(new BaseLevelCommands());
 //        Hotfix.fix(this,"/sdcard/patch.jar");
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Toast.makeText(MyApplication.this,"广告初始化完成，"+initializationStatus.getAdapterStatusMap(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hotfix/activity/MopubActivity.java
+++ app/src/main/java/com/example/hotfix/activity/MopubActivity.java
@@ -1,13 +1,24 @@
 package com.example.hotfix.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hotfix.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
 
 /**
  * <pre>
@@ -17,7 +28,14 @@ import com.example.hotfix.R;
  * </pre>
  */
 public class MopubActivity extends AppCompatActivity implements View.OnClickListener {
+    public String TAG = MopubActivity.class.getSimpleName();
     TextView banner, interstitial, rewardedvideo, nativead;
+    public static final String bannerId = "ca-app-pub-3940256099942544/6300978111";
+    public static final String interstitialId = "ca-app-pub-3940256099942544/1033173712";
+    public static final String rewardedvideoId = "ca-app-pub-3940256099942544/5224354917";
+    private AdView adView;
+    private InterstitialAd mInterstitialAd;
+    private RewardedAd rewardedAd;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,19 +49,139 @@ public class MopubActivity extends AppCompatActivity implements View.OnClickList
         rewardedvideo.setOnClickListener(this);
         nativead = findViewById(R.id.nativead);
         nativead.setOnClickListener(this);
+        adView = findViewById(R.id.adView);
+        adView.setAdListener(new AdListener() {
+
+            @Override
+            public void onAdLoaded() {
+                Log.i(TAG, "Banner - Code to be executed when an ad finishes loading.   Banner adapter class name: " + adView.getResponseInfo().getMediationAdapterClassName());
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Log.i(TAG, "Banner - Code to be executed when an ad request fails.");
+            }
+
+            @Override
+            public void onAdOpened() {
+                Log.i(TAG, "Banner - Code to be executed when an ad opens an overlay that");
+            }
+
+            @Override
+            public void onAdClicked() {
+                Log.i(TAG, "Banner - Code to be executed when the user clicks on an ad.");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                Log.i(TAG, "Banner - Code to be executed when the user has left the app.");
+            }
+
+            @Override
+            public void onAdClosed() {
+                Log.i(TAG, "Banner - Code to be executed when the user is about to return to the app after tapping on an ad.");
+            }
+        });
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Log.i(TAG, "InterstitialAd - Code to be executed when an ad finishes loading.");
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Log.i(TAG, "InterstitialAd - Code to be executed when an ad request fails.");
+            }
+
+            @Override
+            public void onAdOpened() {
+                Log.i(TAG, "InterstitialAd - Code to be executed when an ad opens an overlay that");
+            }
+
+            @Override
+            public void onAdClicked() {
+                Log.i(TAG, "InterstitialAd - Code to be executed when the user clicks on an ad.");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                Log.i(TAG, "InterstitialAd - Code to be executed when the user has left the app.");
+            }
+
+            @Override
+            public void onAdClosed() {
+                Log.i(TAG, "InterstitialAd - Code to be executed when the user is about to return.");
+            }
+        });
+        rewardedAd = createAndLoadRewardedAd();
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.banner:
+                AdRequest adRequest = new AdRequest.Builder().build();
+                adView.loadAd(adRequest);
                 break;
             case R.id.interstitial:
+                mInterstitialAd.setAdUnitId(interstitialId);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
                 break;
             case R.id.rewardedvideo:
+                if (rewardedAd.isLoaded()) {
+                    RewardedAdCallback adCallback = new RewardedAdCallback() {
+                        @Override
+                        public void onRewardedAdOpened() {
+                            Log.i(TAG, "RewardedAd - Ad opened.");
+                        }
+
+                        @Override
+                        public void onRewardedAdClosed() {
+                            Log.i(TAG, "RewardedAd - Ad closed.");
+                            rewardedAd = createAndLoadRewardedAd();
+                        }
+
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem reward) {
+                            Log.i(TAG, "RewardedAd - User earned reward.");
+                        }
+
+                        @Override
+                        public void onRewardedAdFailedToShow(int errorCode) {
+                            Log.i(TAG, "RewardedAd - Ad failed to display.");
+                        }
+                    };
+                    rewardedAd.show(MopubActivity.this, adCallback);
+                }
                 break;
             case R.id.nativead:
+//                Intent intent= new Intent(MopubActivity.this,NativeActivity.class);
+//                startActivity(intent);
                 break;
         }
     }
+
+    public RewardedAd createAndLoadRewardedAd() {
+        RewardedAd rewardedAd = new RewardedAd(this, rewardedvideoId);
+        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+            @Override
+            public void onRewardedAdLoaded() {
+                Log.i(TAG, "RewardedAd - Ad successfully loaded.");
+            }
+
+            @Override
+            public void onRewardedAdFailedToLoad(int errorCode) {
+                Log.i(TAG, "RewardedAd - Ad failed to load.");
+            }
+        };
+        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+        return rewardedAd;
+    }
+
+
 }
