e99ddaa0ea678d0e4197a4474bb9b347b2b7440a
jian <moruoyiming123@gmail.com>
2020-07-07 08:16:00 +0800

    完善FlowLayout

--- app/src/main/java/com/example/hotfix/widget/FlowLayout.java
+++ app/src/main/java/com/example/hotfix/widget/FlowLayout.java
@@ -63,55 +63,52 @@ public class FlowLayout extends ViewGroup {
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
-//            if (child.getVisibility() == View.GONE) {
-//                continue;
-//            }
-
             LayoutParams layoutParams = child.getLayoutParams();
-            //widthMeasureSpec 为父类传入
-            //padding 获取父类 padding
-            //layoutParams.width 需要的值
-            int childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec, paddingleft + paddingright, layoutParams.width);
-            int childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec, paddingtop + paddingbottom, layoutParams.height);
-            //传入的只是参考值
-            child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+            if (child.getVisibility() != View.GONE) {
+                //widthMeasureSpec 为父类传入
+                //padding 获取父类 padding
+                //layoutParams.width 需要的值
+                int childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec, paddingleft + paddingright, layoutParams.width);
+                int childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec, paddingtop + paddingbottom, layoutParams.height);
+                //传入的只是参考值
+                child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
 
 
-            //获取子view度量宽高
-            int childMeasureWidth = child.getMeasuredWidth();
-            int childMeasureHeight = child.getMeasuredHeight();
+                //获取子view度量宽高
+                int childMeasureWidth = child.getMeasuredWidth();
+                int childMeasureHeight = child.getMeasuredHeight();
 
 
-            //换行操作
-            if ((childMeasureWidth + lineWidthUsed + lineSpacing) > selfWidth) {//超过一行 换行 清数据
+                //换行操作
+                if ((childMeasureWidth + lineWidthUsed + lineSpacing) > selfWidth) {//超过一行 换行 清数据
 
-                //换行，记录
-                allLines.add(views);
-                lineHeghts.add(lineHeight);
+                    //换行，记录
+                    allLines.add(views);
+                    lineHeghts.add(lineHeight);
 
-                parentNeededWidth = Math.max(parentNeededWidth, lineWidthUsed + lineSpacing);
-                parentNeededHeight = parentNeededHeight + lineHeight + itemSpacing;
-                views.clear();
-                lineWidthUsed = 0;
-                lineHeight = 0;
+                    parentNeededWidth = Math.max(parentNeededWidth, lineWidthUsed + lineSpacing);
+                    parentNeededHeight = parentNeededHeight + lineHeight + itemSpacing;
+                    views = new ArrayList<>();
+                    lineWidthUsed = 0;
+                    lineHeight = 0;
 
+                }
+
+
+                //view是分行layout的，所以要记录每一行有哪些view，这样可以方便layout布局
+                views.add(child);
+                lineWidthUsed = lineWidthUsed + childMeasureWidth + lineSpacing;
+                lineHeight = Math.max(lineHeight, childMeasureHeight);
+
+
+                //处理最后一行数据
+                if (i == childCount - 1) {
+                    allLines.add(views);
+                    lineHeghts.add(lineHeight);
+                    parentNeededHeight = parentNeededHeight + lineHeight + itemSpacing;
+                    parentNeededWidth = Math.max(parentNeededWidth, lineWidthUsed + lineSpacing);
+                }
             }
-
-
-            //view是分行layout的，所以要记录每一行有哪些view，这样可以方便layout布局
-            views.add(child);
-            lineWidthUsed = lineWidthUsed + childMeasureWidth + lineSpacing;
-            lineHeight = Math.max(lineHeight, childMeasureHeight);
-
-
-            //处理最后一行数据
-            if (i == childCount - 1) {
-                allLines.add(views);
-                lineHeghts.add(lineHeight);
-                parentNeededHeight = parentNeededHeight + lineHeight + itemSpacing;
-                parentNeededWidth = Math.max(parentNeededWidth, lineWidthUsed + lineSpacing);
-            }
-
         }
 
         //2.度量自己 并保存   所有行 就是高，最宽的 为宽
