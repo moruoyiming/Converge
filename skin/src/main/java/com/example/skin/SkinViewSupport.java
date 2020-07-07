e8df8f34aafcbe12673543e22cefb627566ed835
jian <moruoyiming123@gmail.com>
2020-07-08 07:26:12 +0800

    完善FlowLayout

---
+++ skin/src/main/java/com/example/skin/SkinViewSupport.java
@@ -0,0 +1,5 @@
+package com.example.skin;
+
+public interface SkinViewSupport {
+    void apply();
+}
---
+++ skin/src/main/java/com/example/skin/utils/SkinResources.java
@@ -0,0 +1,7 @@
+package com.example.skin.utils;
+
+public class SkinResources {
+
+    private String mSkinPackage;
+    private boolean isDefaultSkin;
+}
