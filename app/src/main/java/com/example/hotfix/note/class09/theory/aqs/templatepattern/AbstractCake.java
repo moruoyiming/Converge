b4bd5d6a792bf7c4cac22aca74009e205c2fcffa
jian <moruoyiming123@gmail.com>
2020-07-12 11:13:36 +0800

    AQS原理 CLH思想

---
+++ app/src/main/java/com/example/hotfix/note/class09/theory/aqs/templatepattern/AbstractCake.java
@@ -0,0 +1,18 @@
+package com.example.hotfix.note.class09.theory.aqs.templatepattern;
+
+/**
+ * 类说明：抽象蛋糕模型
+ */
+public abstract class AbstractCake {
+    protected abstract void shape();/*造型*/
+    protected abstract void apply();/*涂抹*/
+    protected abstract void brake();/*烤面包*/
+
+    /*做个蛋糕 */
+    public final void run(){
+        this.shape();
+        this.apply();
+        this.brake();
+    }
+
+}
