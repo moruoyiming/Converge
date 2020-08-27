3f39e25d6a5d592c0f075e28a7a1c8a3986a6749
jian <moruoyiming123@gmail.com>
2020-08-27 21:47:05 +0800

    binder进程间通讯原理

---
+++ app/src/main/java/com/example/hotfix/note/binder/demo/server/RemoteService.java
@@ -0,0 +1,40 @@
+package com.example.hotfix.note.binder.demo.server;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.hotfix.note.binder.demo.bean.Person;
+import com.example.hotfix.note.binder.demo.common.Stub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoteService extends Service {
+
+    private ArrayList<Person> persons;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        persons = new ArrayList<>();
+        Log.e("LeoAidlService", "success onBind");
+        return iBinder;
+    }
+
+    private IBinder iBinder = new Stub() {
+        @Override
+        public void addPerson(Person person) throws RemoteException {
+            persons.add(person);
+        }
+
+        @Override
+        public List<Person> getPersonList() throws RemoteException {
+            return persons;
+        }
+    };
+}
