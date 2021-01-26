530e704fcfee599eb7d13df749ca9d502b2a8c98
jian <moruoyiming123@gmail.com>
2021-01-27 00:18:11 +0800

    workmanager学习

---
+++ app/src/main/java/com/example/converge/note/jetpack/workmanager/WorkManager1.java
@@ -0,0 +1,22 @@
+package com.example.converge.note.jetpack.workmanager;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class WorkManager1 extends Worker {
+
+    public WorkManager1(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i("WorkManager", "doWork: dowork !");
+        return Result.success();
+    }
+}
--- app/src/main/java/com/example/converge/note/jetpack/workmanager/WorkManagerActivity.java
+++ app/src/main/java/com/example/converge/note/jetpack/workmanager/WorkManagerActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import com.example.converge.R;
 
@@ -14,5 +16,9 @@ public class WorkManagerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workmanager);
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest
+                .Builder(WorkManager1.class)
+                .build();
+        WorkManager.getInstance(this).enqueue(oneTimeWorkRequest);
     }
 }
