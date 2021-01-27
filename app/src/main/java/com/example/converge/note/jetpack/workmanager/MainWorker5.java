9ce09a8c4f52b715c6c734257fa42f0aabc00f33
jianruilin <798774875@qq.com>
2021-01-27 10:40:00 +0800

    workmanager学习

---
+++ app/src/main/java/com/example/converge/note/jetpack/workmanager/MainWorker5.java
@@ -0,0 +1,37 @@
+package com.example.converge.note.jetpack.workmanager;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+/**
+ * 后台任务5
+ */
+public class MainWorker5 extends Worker {
+
+    public final static String TAG = MainWorker5.class.getSimpleName();
+
+    private Context mContext;
+    private WorkerParameters workerParams;
+
+    // 有构造函数
+    public MainWorker5(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.mContext = context;
+        this.workerParams = workerParams;
+    }
+
+    @SuppressLint("RestrictedApi")
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        Log.d(TAG, "MainWorker5 doWork: 后台任务执行了");
+
+        return new Result.Success(); // 本地执行 doWork 任务时 成功 执行任务完毕
+    }
+}
