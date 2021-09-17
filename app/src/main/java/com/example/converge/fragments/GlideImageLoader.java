4655e234361f0169e9133590d8ada2a6279d3b52
moruoyiming <moruoyiming123@gmail.com>
2021-09-17 21:35:33 +0800

    修复 image 圆角问题

--- app/src/main/java/com/example/converge/fragments/GlideImageLoader.java
+++ app/src/main/java/com/example/converge/fragments/GlideImageLoader.java
@@ -1,10 +1,15 @@
 package com.example.converge.fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.shehuan.niv.NiceImageView;
@@ -12,9 +17,31 @@ import com.shehuan.niv.NiceImageView;
 public class GlideImageLoader extends ImageLoader {
 
     @Override
-    public void displayImage(Context context, Object path, ImageView niceImageView) {
+    public void displayImage(Context context, Object path, ImageView imageView) {
 
-       Glide.with(context).load((String)path).apply(new RequestOptions().transform(new RoundedCorners(30))).into(niceImageView);
+        RequestOptions options = new RequestOptions();
+        options.transform(buildRoundCornerTransformation(imageView, 20));
+        Glide.with(imageView.getContext()).load((String)path).apply(options).into(imageView);
+    }
 
+    /**
+     * Glide 圆角问题
+     * @param imageView
+     * @param cornerRadius
+     * @return
+     */
+    private static MultiTransformation buildRoundCornerTransformation(ImageView imageView, int cornerRadius) {
+        switch (imageView.getScaleType()) {
+            case CENTER_INSIDE:
+                return new MultiTransformation<Bitmap>(new CenterInside(), new RoundedCorners(cornerRadius));
+            case FIT_CENTER:
+            case FIT_START:
+            case FIT_END:
+            case FIT_XY:
+                return new MultiTransformation<Bitmap>(new FitCenter(), new RoundedCorners(cornerRadius));
+            case CENTER_CROP:
+            default:
+                return new MultiTransformation<Bitmap>(new CenterCrop(), new RoundedCorners(cornerRadius));
+        }
     }
 }
