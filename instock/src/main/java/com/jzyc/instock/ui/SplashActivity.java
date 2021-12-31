e107fa56ae331d4fab315543be2170a07fa8c03b
Rui Lin  Jian <moruoyiming123@gmail.com>
2021-12-31 13:49:09 +0800

    增加自定义控件

---
+++ instock/src/main/java/com/jzyc/instock/ui/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.jzyc.instock.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @Date: 2021/12/31
+ * @Time: 12:56 下午
+ * @Author: Jian
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
